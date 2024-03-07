9061067dde6fe357d959b83de614a3eacfacffdb
CGil4444 <gilchristopher404@gmail.com>
2024-03-06 21:00:30 -0500

    Added Footer object with image hyperlinks. Did more tweaks to About section layout and CSS.

---
+++ client/src/app/components/footer/footer.js
@@ -0,0 +1,22 @@
+"use client"
+
+import React, { useState, useEffect } from 'react';
+import './footer.css';
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-container">
+                <p>
+                    2024 Queens University Dance Marathon. Visit our social media pages too! 
+                </p>
+                <a href="https://www.instagram.com/qudancemarathon/?hl=en" target="_blank" rel="noopener noreferrer">
+                    <img src="images/logo/logo_instagram.png" alt="logo" className="logo-img"></img>
+                </a>
+                <a href="https://linktr.ee/qudancemarathon" target="_blank" rel="noopener noreferrer">
+                    <img src="images/logo/logo_linktree.png" alt="logo" className="logo-img"></img>
+                </a>
+            </div>
+        </footer>
+    )
+}
+export default Footer;
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Nav from "./components/nav/nav.js";
+import Footer from "./components/footer/footer.js";
 import './home.css';
 export default function Home() {
   return (
@@ -23,34 +24,44 @@ export default function Home() {
 
       {/*ABOUT SECTION*/}
       <section className="about-section">
-        <div className="about-container">
-          <div className="about-content">
-            {/*display image here*/}
-            <div className="image-container">
-              {/*Using online sample for now. can use local files as well*/}
-              <Image src="https://images.unsplash.com/photo-1581260466152-d2c0303e54f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=50" alt="Blank Image" width={500} height={250} />
-            </div>
-            {/*About Text*/}
+      <div className="about-container">
+        <div className="left">
+          <div className="image-container">
+            {/* Using online sample for now. can use local files as well */}
+            <Image src="https://images.unsplash.com/photo-1581260466152-d2c0303e54f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=50" alt="Blank Image" width={500} height={250} />
+          </div>
+        </div>
+        <div className="right">
+          <div className="top-right">
             <div className="text-container">
               <h2>About QUDM</h2>
               <p>
-              Queen's University Dance Marathon is a student-run club aiming to raise funds and awareness for the Children's Miracle Network! 
-              100% of QUDM's efforts provide hope & care for the kids at the Children's Hospital of Eastern Ontario (CHEO)! 
-              The funds raised by our events help cover the cost of medical equipment, charitable care, medical research, and patient services.
+                Queen's University Dance Marathon is a student-run club aiming to raise funds and awareness for the Children's Miracle Network! 
+                100% of QUDM's efforts provide hope & care for the kids at the Children's Hospital of Eastern Ontario (CHEO)! 
+                The funds raised by our events help cover the cost of medical equipment, charitable care, medical research, and patient services.
               </p>
-              {/*Subsection with 5 images... i should probably make these an array at some point*/}
-              <div className="image-gallery">
+            </div>
+          </div>
+          <div className="bottom-right">
+            {/* Add content for the bottom-right section here if needed */}
+            {/* Subsection with 5 images... i should probably make these an array at some point */}
+            <div className="image-gallery">
                 <Image src="https://images.unsplash.com/photo-1581260466152-d2c0303e54f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=50" alt="Image 1" width={100} height={100} />
                 <Image src="https://images.unsplash.com/photo-1581260466152-d2c0303e54f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=50" alt="Image 2" width={100} height={100} />
                 <Image src="https://images.unsplash.com/photo-1581260466152-d2c0303e54f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=50" alt="Image 3" width={100} height={100} />
                 <Image src="https://images.unsplash.com/photo-1581260466152-d2c0303e54f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=50" alt="Image 4" width={100} height={100} />
                 <Image src="https://images.unsplash.com/photo-1581260466152-d2c0303e54f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=50" alt="Image 5" width={100} height={100} />
-              </div>
             </div>
           </div>
         </div>
-      </section>
-      {/*END OF ABOUT SECTION*/}
+      </div>
+    </section>
+    {/*END OF ABOUT SECTION*/}
+
+    {/*FOOTER SECTION*/}
+    <div style={{ height: '200px' }} />
+    <Footer/>
+    {/*FOOTER SECTION*/}
     </div>
   );
 }
