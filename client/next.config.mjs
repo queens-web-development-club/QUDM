d985fddb1909256b455bbf1e0607a6925627d59f
CGil4444 <gilchristopher404@gmail.com>
2024-04-07 03:43:10 -0400

    Forgor 2 push. This just redirects calls from Gallery.js to get da images

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.unsplash.com'],//added this just so i can use sample images on the frontpage
+        domains: ['images.unsplash.com'], // added this just so I can use sample images on the front page
     }
 };
 
-export default nextConfig;
+export default {
+    ...nextConfig,
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://localhost:3002/api/:path*', // Change this URL to match your backend server URL
+            },
+        ];
+    },
+};
